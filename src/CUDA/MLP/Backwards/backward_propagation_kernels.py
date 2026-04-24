62ee4b153487a7ae66de061504b5b0830911c720
Kevin Fripp <96999054+KevinGFripp@users.noreply.github.com>
2026-04-24 22:53:51 +0100

    Updated python kernels

--- src/CUDA/MLP/Backwards/backward_propagation_kernels.py
+++ src/CUDA/MLP/Backwards/backward_propagation_kernels.py
@@ -8,10 +8,21 @@ hidden_layer_gradient_kernel = RawKernel(
                         'hidden_layer_gradient_kernel')
 hidden_layer_gradient_kernel.compile()
 
+hidden_layer_gradient_wmma_kernel = RawKernel(
+    Path('src/CUDA/MLP/Backwards/Kernels/hidden_layer_gradient_wmma_kernel.cu').read_text(),
+                        'hidden_layer_gradient_wmma_kernel')
+hidden_layer_gradient_wmma_kernel.compile()
+
 weight_gradient_kernel = RawKernel(
     Path('src/CUDA/MLP/Backwards/Kernels/weight_gradient_kernel.cu').read_text(),
                         'weight_gradient_kernel')
 weight_gradient_kernel.compile()
+
+
+weight_gradient_wmma_kernel = RawKernel(
+    Path('src/CUDA/MLP/Backwards/Kernels/weight_gradient_wmma_kernel.cu').read_text(),
+                        'weight_gradient_wmma_kernel')
+weight_gradient_wmma_kernel.compile()
 
 
 def hidden_layer_gradient(W: ndarray, gradient: ndarray, this_gradient: ndarray, this_z: ndarray):
@@ -31,6 +42,23 @@ def hidden_layer_gradient(W: ndarray, gradient: ndarray, this_gradient: ndarray,
                                    M, N, K))
     return
 
+def hidden_layer_gradient_wmma(W: ndarray, gradient: ndarray, this_gradient: ndarray, this_z: ndarray):
+
+    M = W.shape[1]
+    N = gradient.shape[1]
+    K = W.shape[0]
+
+    norm_factor = float32(1./N)
+
+    hidden_layer_gradient_wmma_kernel(*kernel_config(M, N),
+                               (W.data.ptr,
+                                   gradient.data.ptr,
+                                   this_gradient.data.ptr,
+                                   this_z.data.ptr,
+                                   norm_factor,
+                                   M, N, K))
+    return
+
 def weight_gradient(this_gradient: ndarray, previous_activation: ndarray, dW: ndarray):
 
     M = this_gradient.shape[0]
@@ -40,6 +68,22 @@ def weight_gradient(this_gradient: ndarray, previous_activation: ndarray, dW: nd
     norm_factor = float32(1./N)
 
     weight_gradient_kernel(*kernel_config(M, N),
+                           (this_gradient.data.ptr,
+                                 previous_activation.data.ptr,
+                                 dW.data.ptr,
+                                 norm_factor,
+                                 M, N, K))
+    return
+
+def weight_gradient_wmma(this_gradient: ndarray, previous_activation: ndarray, dW: ndarray):
+
+    M = this_gradient.shape[0]
+    N = previous_activation.shape[0]
+    K = this_gradient.shape[1]
+
+    norm_factor = float32(1./N)
+
+    weight_gradient_wmma_kernel(*kernel_config(M, N),
                            (this_gradient.data.ptr,
                                  previous_activation.data.ptr,
                                  dW.data.ptr,
