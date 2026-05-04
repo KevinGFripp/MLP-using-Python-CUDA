7b40f7a3499da4ae9d758f379eaaf6457b985aea
Kevin Fripp <96999054+KevinGFripp@users.noreply.github.com>
2026-05-04 23:00:50 +0100

    Updated Python activation kernels

--- src/CUDA/MLP/Activations/activation_kernels.py
+++ src/CUDA/MLP/Activations/activation_kernels.py
@@ -8,6 +8,11 @@ relu_kernel = RawKernel(
                         'ReLU_kernel')
 relu_kernel.compile()
 
+softmax_10_kernel = RawKernel(
+    Path('src/CUDA/MLP/Activations/Kernels/softmax_10_kernel.cu').read_text(),
+                        'softmax_10_kernel')
+softmax_10_kernel.compile()
+
 def relu(z:ndarray, a:ndarray):
     M,N = z.shape
 
@@ -17,10 +22,26 @@ def relu(z:ndarray, a:ndarray):
                     M*N))
     return
 
+def softmax_10(z:ndarray, a:ndarray):
+    M,N = z.shape
+
+    softmax_10_kernel(*softmax_10_kernel_config(N),
+                (z.data.ptr,
+                      a.data.ptr,
+                      N))
+    return
+
 
 def kernel_config(size):
 
     threads = 256
     blocks = (size + threads -1)//threads
+
+    return (blocks,),(threads,)
+
+def softmax_10_kernel_config(batch_size):
+
+    threads = 256
+    blocks = (batch_size + 16 -1)//16
 
     return (blocks,),(threads,)
